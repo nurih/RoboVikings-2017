93fd1cc5fe456c709ca567cfedd6cabf3bdda8d9
Coder <coder@robovikings.edu>
2017-10-28 11:14:50 -0700

    Jewel changed to linear op mode. Jewel knocker working. Driving a bit after knocking jewel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelOpMode.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -9,16 +10,26 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 
 @Autonomous(name = "Jewel", group = "Test")
-public class JewelOpMode extends OpMode {
-    final float FULLPOWER = 1;
+public class JewelOpMode extends LinearOpMode {
+    public static final double START_POSITION = .45;
+    final float FULLPOWER = 0.5f;
     NormalizedColorSensor colorSensor;
 
+    Alliance allience = Alliance.Red;
     private Servo servo = null;
     private DcMotor rightMotor = null;
     private DcMotor leftMotor = null;
 
-    @Override
-    public void init() {
+    /**
+     * Override this method and place your code here.
+     * <p>
+     * Please do not swallow the InterruptedException, as it is used in cases
+     * where the op mode needs to be terminated early.
+     *
+     * @throws InterruptedException
+     */
+
+    public void setup() {
         colorSensor = Viki.getRobotPart(hardwareMap, RobotPart.colorSensor);
         servo = Viki.getRobotPart(hardwareMap, RobotPart.jewelServo);
         rightMotor = Viki.getRobotPart(hardwareMap, RobotPart.rightMotor);
@@ -27,47 +38,90 @@ public class JewelOpMode extends OpMode {
         leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        servo.setPosition(Servo.MAX_POSITION);
-telemetry.addLine("Initialized!");
+        servo.setPosition(START_POSITION);
+
+        telemetry.addLine("Initialized!");
     }
 
+
     @Override
-    public void loop() {
+    public void runOpMode() throws InterruptedException {
+        setup();
 
-        HsvValues hsv = Viki.getHsvValues(colorSensor);
-
-        boolean isRed = getIsRed(hsv);
-        boolean isBlue = getIsBlue(hsv);
-
+        // Lower arm
         servo.setPosition(Servo.MAX_POSITION);
 
-        if ( isBlue ) {
-            leftMotor.setPower(FULLPOWER);
-            rightMotor.setPower(FULLPOWER);
+        // detect color
+        boolean isRed;
+        boolean isBlue;
+        do {
+            HsvValues hsv = Viki.getHsvValues(colorSensor);
+            isRed = getIsRed(hsv);
+            isBlue = getIsBlue(hsv);
 
-        } else if ( isRed ) {
-            leftMotor.setPower(-FULLPOWER);
-            rightMotor.setPower(-FULLPOWER);
+            telemetry.addData("Hsv.Hue", hsv.Hue);
+            telemetry.addData("Hsv.Value", hsv.Value);
+            telemetry.addData("Seeing Blue ", isBlue);
+            telemetry.addData("Seeing Red ", isRed);
+        } while (isRed == false && isBlue == false);
 
+
+        // "react" - knock off the jewel according to our alliance
+        if (allience == Alliance.Red) {
+            if (isBlue) {
+                goForward();
+            } else if (isRed) {
+                goBackward();
+            } else {
+                stopMotors();
+            }
         } else {
-            leftMotor.setPower(0);
-            rightMotor.setPower(0);
+            if (isBlue) {
+                goBackward();
+            } else if (isRed) {
+                goForward();
+            } else {
+                stopMotors();
+            }
         }
 
-        telemetry.addData("Hsv.Hue", hsv.Hue);
-        telemetry.addData("Hsv.Value", hsv.Value);
 
-        telemetry.addData("Seeing Blue ", isBlue);
-        telemetry.addData("Seeing Red ", isRed);
+        // lift arm again
 
+        servo.setPosition(START_POSITION);
 
+        // driver forward a bit
+
+        double startTime = super.getRuntime();
+        do {
+            goForward();
+        } while (getRuntime() < startTime + 2);
+
+        // stop motors
+        stopMotors();
+
+    }
+
+    private void stopMotors() {
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+    }
+
+    private void goBackward() {
+        leftMotor.setPower(-FULLPOWER);
+        rightMotor.setPower(-FULLPOWER);
+    }
+
+    private void goForward() {
+        leftMotor.setPower(FULLPOWER);
+        rightMotor.setPower(FULLPOWER);
     }
 
     private boolean getIsBlue(HsvValues hsv) {
         boolean result;
-        if ( hsv.Hue < 180 ) {
+        if (hsv.Hue < 180) {
             result = false;
-        } else if ( hsv.Hue > 220 ) {
+        } else if (hsv.Hue > 220) {
             result = false;
         } else {
             result = true;
@@ -78,9 +132,9 @@ telemetry.addLine("Initialized!");
     private boolean getIsRed(HsvValues hsv) {
 
         boolean result;
-        if ( hsv.Hue < 5 ) {
+        if (hsv.Hue < 5) {
             result = true;
-        } else if ( hsv.Hue > 320 ) {
+        } else if (hsv.Hue > 320) {
             result = true;
         } else {
             result = false;
