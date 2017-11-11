fa518dc19f2d198b2c55dec7f78bf2bff3d8d80e
coder <dev@robovikings.com>
2017-11-10 16:45:38 -0800

    driving mode fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
@@ -13,24 +13,35 @@ public class DriveOpMode extends OpMode {
 
     @Override
     public void init() {
-        rightMotor = Viki.getRobotPart(hardwareMap, RobotPart.rightMotor);
-        leftMotor = Viki.getRobotPart(hardwareMap, RobotPart.leftMotor);
-        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        setMotorsForForward();
         rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
+    private void setMotorsForForward() {
+        rightMotor = Viki.getRobotPart(hardwareMap, RobotPart.rightMotor);
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftMotor = Viki.getRobotPart(hardwareMap, RobotPart.leftMotor);
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    private void setMotorsForReverse() {
+        rightMotor = Viki.getRobotPart(hardwareMap, RobotPart.leftMotor);
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftMotor = Viki.getRobotPart(hardwareMap, RobotPart.rightMotor);
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
     @Override
     public void loop() {
-        leftMotor.setPower(gamepad1.left_stick_y);
-        rightMotor.setPower(gamepad1.right_stick_y);
-        if (gamepad1.x) {
-            leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        } else if (gamepad1.y) {
-            leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-            rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotor.setPower(gamepad1.left_stick_y * 0.7);
+        rightMotor.setPower(gamepad1.right_stick_y * 0.7);
+        if ( gamepad1.x ) {
+            setMotorsForForward();
+        } else if ( gamepad1.y ) {
+            setMotorsForReverse();
         }
     }
 }
